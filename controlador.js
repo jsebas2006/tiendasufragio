20defb6729338858ff48322d93b48964a4cb7f4c
sbas <sebas.202120@gmail.com>
2022-05-12 17:23:04 -0500

    f

--- controlador.js
+++ controlador.js
@@ -1,108 +1,455 @@
 //OBJETOS
 let producto1={
-    nombre:"producto1",
-    precio:500000,
+    nombre:"secretos dumbledor",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/0138242.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=8ae9e152-7515-4294-a236-393fc11b0559"
 }
 
 let producto2={
-    nombre:"producto2",
-    precio:500000,
+    nombre:"tipos malos",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/0496448.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=659d6c65-0430-4bc8-b0e9-4c055be0b3be"
 }
 
 let producto3={
-    nombre:"producto3",
-    precio:500000,
+    nombre:"doctor Strange ",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/1839314.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=25da8356-075d-4d57-98e3-c2296b9d247e"
 }
 
 let producto4={
-    nombre:"producto4",
-    precio:500000,
+    nombre:"rainman",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/20425603.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=3e0ecc25-fc36-46f2-a5cb-e32be81881d9"
 }
 
 let producto5={
-    nombre:"producto5",
-    precio:500000,
+    nombre:"el hombre del norte",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/2402670.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=dfaf4790-7b68-41b5-a3f3-65cecde070a2"
 }
 
 let producto6={
-    nombre:"producto6",
-    precio:500000,
+    nombre:"batman",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/2914301.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=bfa79008-3d39-4967-a046-9b01e2109aee"
 }
 
 let producto7={
-    nombre:"producto7",
-    precio:500000,
+    nombre:"alcarras",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/3832685.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=671ec80c-6146-42e6-a8fe-35e8c8551875"
 }
 
 let producto8={
-    nombre:"producto8",
-    precio:500000,
+    nombre:"x",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/4872294.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=43e6dc8b-afd7-482e-8818-57fd67bee797"
 }
 
 let producto9={
-    nombre:"producto9",
-    precio:500000,
+    nombre:"sonic 2",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/5195258.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=353d537b-a361-41f1-b813-c73aed8b3d70"
 }
 
 let producto10={
-    nombre:"producto10",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"veneciafrena",
+    precio:20000,
+    estado:"Nuevo",pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/5562778.jpg-c_150_200_x-f_jpg-q_x-xxyxx.jpg?alt=media&token=58e8fd3a-11ae-404a-8a7d-67014da73a59"
 }
 
 let producto11={
-    nombre:"producto11",
-    precio:500000,
+    nombre:"proyecto rampage",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/descarga%20(1).jfif?alt=media&token=77d40ccf-dec8-4cd2-8d3b-9511a4eb9e29"
 }
 
 let producto12={
-    nombre:"producto12",
-    precio:500000,
+    nombre:"los increibles 2",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/descarga%20(2).jfif?alt=media&token=2a6d6c98-180b-4f6e-9ab0-84887966ddfa"
 }
 
 let producto13={
-    nombre:"producto13",
-    precio:500000,
+    nombre:"la ciudad perdida ",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/descarga.jfif?alt=media&token=8ea5ee6c-0fc6-4092-a5c0-211cabd73f16"
 }
 
 let producto14={
-    nombre:"producto14",
-    precio:500000,
+    nombre:"space",
+    precio:20000,
     estado:"Nuevo",
-    foto:""
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/images%20(1).jfif?alt=media&token=2aed5da1-9bda-4e1d-87da-e7ce21bb82ff"
 }
 
 let producto15={
-    nombre:"producto15",
-    precio:500000,
+    nombre:"scream",
+    precio:20000,
     estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/images%20(2).jfif?alt=media&token=911ae860-be53-470a-9c79-461000a12853"
+}
+
+let producto16={
+    nombre:"shrek",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/images%20(3).jfif?alt=media&token=2f1fe32c-11c4-4f46-8a2c-9db0d8ecb980"
+}
+
+let producto17={
+    nombre:"uncharted",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/images%20(4).jfif?alt=media&token=4b5628ca-458f-4d73-902b-33381a77dea7"
+}
+
+let producto18={
+    nombre:"alerta roja",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/images.jfif?alt=media&token=165db3d3-f04d-44f4-95d8-37469cdb3bbb"
+}
+
+let producto19={
+    nombre:"quarentine 2",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/images%20(2).jfif?alt=media&token=ca2e1886-3da0-41d6-b4b9-b303bba97769"
+}
+
+let producto20={
+    nombre:"el hoyo",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-9abd0.appspot.com/o/images%20(1).jfif?alt=media&token=55d9ce52-7abc-4ac5-a067-ccc727a6d7ca"
+}
+
+let producto21={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
     foto:""
 }
+
+let producto22={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto23={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+
+
+let producto24={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto25={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto26={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto27={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto28={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto29={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto30={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto31={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto32={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto33={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto34={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto35={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto36={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto37={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto38={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto39={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto40={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto41={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto42={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto43={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto44={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto45={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto46={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto47={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto48={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto49={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
+let producto50={
+    nombre:"",
+    precio:20000,
+    estado:"Nuevo",
+    pais:"colombia",
+    popularidad:"⭐⭐⭐⭐⭐",
+    foto:""
+}
+
 
 //ARREGLO
 let productos=Array(
@@ -120,11 +467,87 @@ let productos=Array(
     producto12,
     producto13,
     producto14,
-    producto15
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto24,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+    producto31,
+    producto32,
+    producto33,
+    producto34,
+    producto35,
+    producto36,
+    producto37,
+    producto38,
+    producto39,
+    producto40,
+    producto41,
+    producto42,
+    producto43,
+    producto44,
+    producto45,
+    producto46,
+    producto47,
+    producto48,
+    producto49,
+    producto50,
 )
 
-//RECORRO EL ARREGLO
-productos.forEach(function(producto){
-    console.log(producto.nombre)
-})
 
+//RECORRO EL ARREGLO
+let contenedor=document.getElementById("contenedor")
+productos.forEach(function(producto){
+    
+    let foto=document.createElement("img")
+    foto.classList.add("w-100","img-fluid")
+    foto.src=producto.foto
+
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100")
+
+    let nombreproducto=document.createElement("h1")
+    nombreproducto.classList.add("text-center")
+    nombreproducto.textContent=producto.nombre
+
+    let precioproducto=document.createElement("h1")
+    precioproducto.classList.add("text-center")
+    precioproducto.textContent=("$")+producto.precio
+
+    let estadoproducto=document.createElement("h1")
+    estadoproducto.classList.add("text-center")
+    estadoproducto.textContent=producto.estado
+
+    let paisproducto=document.createElement("h1")
+    paisproducto.classList.add("text-center")
+    paisproducto.textContent=producto.pais
+
+    let popularidadproducto=document.createElement("h1")
+    popularidadproducto.classList.add("text-center")
+    popularidadproducto.textContent=producto.popularidad
+
+
+    tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombreproducto)
+    tarjeta.appendChild(precioproducto)
+    tarjeta.appendChild(estadoproducto)
+    tarjeta.appendChild(paisproducto)
+    tarjeta.appendChild(popularidadproducto)
+    columna.appendChild(tarjeta)
+    contenedor.appendChild(columna)
+
+})
